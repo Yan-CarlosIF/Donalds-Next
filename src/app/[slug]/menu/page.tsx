722e65b46e519c6f0b2a9058220b16bd396b4673
Yan Carlos <yan312005@gmail.com>
2025-02-20 12:03:12 -0300

    feat: add unique constraint to restaurant slug and initial menu page

---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,35 @@
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import RestaurantHeader from "./components/header";
+
+interface RestaurantMenuPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
+}
+
+const isConsumptionMethodValid = (consumptionMethod: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+};
+
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = await params;
+  const { consumptionMethod } = await searchParams;
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+
+  if (!restaurant || !isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      <RestaurantHeader restaurant={restaurant} />
+    </div>
+  );
+};
+
+export default RestaurantMenuPage;
