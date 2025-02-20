765548093f577bc69bc866295b56e2ad7bf734f6
Yan Carlos <yan312005@gmail.com>
2025-02-20 19:23:05 -0300

    fix: type of ingridients

--- src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -94,7 +94,7 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
               <h4 className="font-semibold">Ingredientes</h4>
             </div>
             <ul className="list-disc px-5 text-sm text-muted-foreground">
-              {product.ingredients.map((ingredient) => (
+              {product.ingredients.map((ingredient: string) => (
                 <li key={ingredient}>{ingredient}</li>
               ))}
             </ul>
