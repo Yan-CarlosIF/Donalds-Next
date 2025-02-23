552602ac6ae2e305165d7fb45ed63f125d5e4ba4
Yan Carlos <yan312005@gmail.com>
2025-02-23 18:20:31 -0300

    feat: ensure order can be created

--- src/app/[slug]/menu/components/finish-order-dialog.tsx
+++ src/app/[slug]/menu/components/finish-order-dialog.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { ConsumptionMethod } from "@prisma/client";
+import { Loader2Icon } from "lucide-react";
+import { useParams, useSearchParams } from "next/navigation";
+import { useContext, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { PatternFormat } from "react-number-format";
+import { toast } from "sonner";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -27,6 +32,9 @@ import {
 import { Input } from "@/components/ui/input";
 import { isValidCpf } from "@/helpers/cpf";
 
+import { createOrder } from "../actions/create-order";
+import { CartContext } from "../contexts/cart";
+
 const formSchema = z.object({
   name: z.string().trim().min(1, { message: "O nome é obrigatório" }),
   cpf: z
@@ -49,6 +57,15 @@ const FinishOrderDialog = ({
   isOpen,
   onOpenChange,
 }: FinishOrderDialogProps) => {
+  const { products } = useContext(CartContext);
+  const { slug } = useParams<{ slug: string }>();
+
+  const consumptionMethod = useSearchParams().get(
+    "consumptionMethod"
+  ) as ConsumptionMethod;
+
+  const [isPending, startTransition] = useTransition();
+
   const form = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -58,8 +75,22 @@ const FinishOrderDialog = ({
     shouldUnregister: true,
   });
 
-  const onSubmit = (data: FormSchema) => {
-    console.log(data);
+  const onSubmit = async (data: FormSchema) => {
+    try {
+      startTransition(async () => {
+        await createOrder({
+          consumptionMethod,
+          customerCpf: data.cpf,
+          customerName: data.name,
+          products,
+          slug,
+        });
+        onOpenChange(false);
+        toast.success("Pedido finalizado com sucesso!");
+      });
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
@@ -111,7 +142,9 @@ const FinishOrderDialog = ({
                   type="submit"
                   variant="destructive"
                   className="rounded-full"
+                  disabled={isPending}
                 >
+                  {isPending && <Loader2Icon className="animate-spin" />}
                   Finalizar
                 </Button>
                 <DrawerClose asChild>
